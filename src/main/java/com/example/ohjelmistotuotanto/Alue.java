11f93a8767cdea338f4189b0a2a535c4e9beadc8
nuuttinyyssonen <nuutti.nyyssonen@gmail.com>
2024-05-02 07:23:28 +0300

    alue luokka

---
+++ src/main/java/com/example/ohjelmistotuotanto/Alue.java
@@ -0,0 +1,17 @@
+package com.example.ohjelmistotuotanto;
+
+public class Alue {
+    private String nimi;
+
+    public Alue(String nimi) {
+        this.nimi = nimi;
+    }
+
+    public String getNimi() {
+        return this.nimi;
+    }
+
+    public void setNimi(String nimi) {
+        this.nimi = nimi;
+    }
+}
